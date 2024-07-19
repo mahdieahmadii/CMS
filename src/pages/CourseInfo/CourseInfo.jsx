d55b4ca5aa3ac95147a0e641f64309fc03e40388
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2024-07-19 17:52:16 +0330

    fix part of components

--- src/pages/CourseInfo/CourseInfo.jsx
+++ src/pages/CourseInfo/CourseInfo.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import Topbar from "../../components/Topbar/Topbar";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+import "./CourseInfo.css";
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 
 export default function CourseInfo() {
-  return <div>CourseInfo</div>;
+  return (
+    <>
+      <Topbar />
+      <Navbar />
+
+      <Breadcrumb
+        links={[
+          { id: 1, title: "خانه", to: "" },
+          {
+            id: 2,
+            title: "آموزش برنامه نویسی فرانت‌اند",
+            to: "category-info/frontend",
+          },
+          {
+            id: 3,
+            title: "دوره متخصص جاوا اسکریپت",
+            to: "course-info/js-expert",
+          },
+        ]}
+      />
+
+      <Footer />
+    </>
+  );
 }
--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -2,8 +2,12 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import LastCourses from "../../components/LastCourses/LastCourses";
 import AboutUs from "../../components/AboutUs/AboutUs";
+import PopularCourses from "../../components/PopularCourses/PopularCourses";
+import PresellCourses from "../../components/PresellCourses/PresellCourses";
+import LastArticles from "../../components/LastArticles/LastArticles";
 
 import "./Index.css";
+import Footer from "../../components/Footer/Footer";
 
 export default function Index() {
   return (
@@ -11,6 +15,10 @@ export default function Index() {
       <Header />
       <LastCourses />
       <AboutUs />
+      <PopularCourses />
+      <PresellCourses />
+      <LastArticles />
+      <Footer />
     </>
   );
 }
